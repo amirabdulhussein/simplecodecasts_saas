f69455f3f341d6d3fef2535b3649e1313162bdb0
= <=>
2015-12-10 12:48:25 +0000

    Added validation and create

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,10 +1,26 @@
 class ContactsController < ApplicationController
     def new      #corresponds to the new.html.erb view
-        @contact = Contact.new
+        @contact = Contact.new # @contact is an instant variable
     end
     
     def create
+        @contact = Contact.new(contact_params)
+        
+        if @contact.save     #runs contact.save and checks if save comes back as ok or not. This saves to the DB
+            flash[:success] = 'Message sent'
+            redirect_to new_contact_path
+        else
+            flash[:danger] = 'Error occured, message has not been sent'
+            redirect_to new_contact_path
+        end
     end
+    
+    private
+        def contact_params
+            params.require(:contact).permit(:name, :email, :comments)
+        end
+    
+    
 end
 
 
