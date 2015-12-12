812492e1f6499a03018503fe4d0f0256155b3117
= <=>
2015-12-12 10:57:49 +0000

    Added QSP

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
     def home     #corresponds to the home.html.erb view
+        @basic_plan = Plan.find(1)   #1 refers to the ID
+        @pro_plan = Plan.find(2)
     end
     
     def about
