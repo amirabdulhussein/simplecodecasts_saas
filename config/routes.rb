14aef412e789cc68af278f73e9920934676f6bf6
= <=>
2015-12-12 07:18:18 +0000

    Added devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   
   resources :contacts
   get '/about' => 'pages#about'
-  root 'pages#home'
+  
+  root 'pages#home'    # go to the pages controller and run the home function
+  
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
